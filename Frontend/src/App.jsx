d32e6442d6b106cce12c17ab4cf485d9ea91fae9
MukhiVivek <vivekmukhi07@gmail.com>
2025-02-13 01:36:39 +0530

    Start The New Project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './landing_page/home/Home'
+import Navbar from "./landing_page/includes/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App;
