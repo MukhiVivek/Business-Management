79777e2baeec0e08fea18b9e05a096fb19e22126
Mukhi Vivek <119511043+MukhiVivek@users.noreply.github.com>
2026-02-06 18:28:41 +0530

    error done

--- Frontend/src/Config.jsx
+++ Frontend/src/Config.jsx
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:4000";
+// export const BACKEND_URL = "http://localhost:4000";
 
-// export const BACKEND_URL = "https://megnet.mukhi.life";
+export const BACKEND_URL = "https://megnet.mukhi.life";
